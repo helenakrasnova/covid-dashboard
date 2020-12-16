32111cd779289698fbf516a999c11105d66a8f88
Helena Krasnova <alenka5354@mail.ru>
2020-12-17 01:09:28 +0300

    feat: implemented all table functionality

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -3,22 +3,82 @@ import TableService from '../../services/TableService';
 export default class Table {
   constructor() {
     this.tableService = new TableService();
+    this.state = {
+      isAbsoluteValues: true,
+      isLatestDay: false,
+    };
   }
 
   async init() {
-    const tableData = await this.tableService.getTotalAbsolute();
+    const tableData = await this.tableService.get(
+      this.state.isAbsoluteValues,
+      this.state.isLatestDay,
+    );
+    this.render(tableData);
+  }
+
+  async update(country) {
+    const tableData = await this.tableService.get(
+      this.state.isAbsoluteValues,
+      this.state.isLatestDay,
+      country,
+    );
+    this.render(tableData);
+  }
+
+  createHeadCell = (text, trHeading) => {
+    const th = document.createElement('th');
+    th.innerHTML = text;
+    trHeading.append(th);
+  }
+
+  createBodyCell = (text, tr) => {
+    const td = document.createElement('td');
+    td.textContent = text;
+    tr.append(td);
+  }
+
+  setState = (newState) => {
+    this.state = { ...this.state, ...newState };
+  }
+
+  onDateRangeClicked = async () => {
+    this.setState({
+      isLatestDay: !this.state.isLatestDay,
+    });
+    const tableData = await this.tableService.get(
+      this.state.isAbsoluteValues,
+      this.state.isLatestDay,
+    );
+    this.render(tableData);
+  }
+
+  onNumberFormatsClicked = async () => {
+    this.setState({
+      isAbsoluteValues: !this.state.isAbsoluteValues,
+    });
+    const tableData = await this.tableService.get(
+      this.state.isAbsoluteValues,
+      this.state.isLatestDay,
+    );
     this.render(tableData);
   }
 
   render = (tableData) => {
     const container = document.querySelector('.container');
-    let covidTable = document.querySelector('.covid-table');
-    if (!covidTable) {
-      covidTable = document.createElement('table');
-      container.append(covidTable);
-      covidTable.className = 'covid-table';
+    let tableComponent = document.querySelector('.table-component');
+    if (!tableComponent) {
+      tableComponent = document.createElement('div');
+      tableComponent.className = 'table-component';
+      container.append(tableComponent);
     }
-    covidTable.innerHTML = '';
+    tableComponent.innerHTML = '';
+    const tableBox = document.createElement('div');
+    tableBox.className = 'table-box';
+    tableComponent.append(tableBox);
+    const covidTable = document.createElement('table');
+    tableBox.append(covidTable);
+    covidTable.className = 'covid-table';
     const trHeading = document.createElement('tr');
     trHeading.className = 'table-heading';
     this.createHeadCell('Country', trHeading);
@@ -35,17 +95,29 @@ export default class Table {
       this.createBodyCell(tableData[i].deaths, tr);
       covidTable.append(tr);
     }
+
+    this.createTableToggle('table-select', this.state.isLatestDay, 'all<br>time');
+    this.createTableToggle('table-selectBy', this.state.isAbsoluteValues, 'abs.');
+    const checkbox = document.querySelector('#table-select');
+    const secondCheckbox = document.querySelector('#table-selectBy');
+    checkbox.addEventListener('click', () => {
+      this.onDateRangeClicked();
+    });
+    secondCheckbox.addEventListener('click', () => {
+      this.onNumberFormatsClicked();
+    });
   }
 
-  createHeadCell = (text, trHeading) => {
-    const th = document.createElement('th');
-    th.innerHTML = text;
-    trHeading.append(th);
-  }
-
-  createBodyCell = (text, tr) => {
-    const td = document.createElement('td');
-    td.textContent = text;
-    tr.append(td);
+  createTableToggle = (inputId, checked, labelText) => {
+    const tableComponent = document.querySelector('.table-component');
+    const input = document.createElement('input');
+    input.type = 'checkbox';
+    input.id = inputId;
+    input.checked = checked;
+    const label = document.createElement('label');
+    label.htmlFor = inputId;
+    label.innerHTML = labelText;
+    tableComponent.append(input);
+    tableComponent.append(label);
   }
 }
