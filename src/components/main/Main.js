5e0cee8ee3a54a13bb035c4cb3343aa0015290a9
Dmitry Shevchenok <dm.shevchenok@gmail.com>
2020-12-21 19:28:35 +0300

    fix: fix some bugs

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,19 +1,16 @@
 import Table from '../table/Table';
 import List from '../list/List';
-import Chart from '../chart/Chart';
-
 export default class Main {
   constructor() {
     this.table = new Table();
     this.list = new List('countries');
-    this.chart = new Chart();
   }
 
   async init() {
     this.render();
     await this.table.init();
     await this.list.initList();
-    await this.chart.initChart();
+
   }
 
   render = () => {
